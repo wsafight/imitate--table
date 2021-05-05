edd534638b118d31607da14ec5a37711a0162b49
wsafight <984292420@qq.com>
2021-05-05 23:44:36 +0800

    chore: 添加依赖

--- src/react-resizable/src/App.tsx
+++ src/react-resizable/src/App.tsx
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {Resizable} from 'react-resizable'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [column, setColumn] = useState([
+        {
+            head: 1,
+            key: 1,
+            width: 100
+        },
+        {
+            head: 2,
+            key: 2,
+            width: 200
+        }
+    ])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    const handleResize = (e: any) => {
+        console.log(e)
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>Hello Vite + React!</p>
+                <table>
+                    <thead>
+                    {
+                        column.map(({head, key, width}, index) => (
+                            <Resizable
+                                key={key}
+                                width={width}
+                                height={0}
+                                onResize={handleResize}
+                            >
+                                <th>{head}</th>
+                            </Resizable>
+                        ))
+                    }
+                    </thead>
+                </table>
+            </header>
+        </div>
+    )
 }
 
 export default App
